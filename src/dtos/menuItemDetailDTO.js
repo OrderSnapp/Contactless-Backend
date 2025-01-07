b1b2e7c15b20da38c281d005060ade75c9419b89
sunchengchhay <sonchengchhay@gmail.com>
2025-01-07 23:19:28 +0700

    update

--- src/dtos/menuItemDetailDTO.js
+++ src/dtos/menuItemDetailDTO.js
@@ -3,7 +3,7 @@ const menuItemDetailDTO = (menuItemDetail) => {
         id: menuItemDetail.id,
         name: menuItemDetail.name,
         price: menuItemDetail.price,
-        status: menuItemDetail.status,
+        status: menuItemDetail.status.charAt(0).toUpperCase() + menuItemDetail.status.slice(1).toLowerCase(),
         ingredient: menuItemDetail.ingredient,
         imageUrl: menuItemDetail.imageUrl
     };
