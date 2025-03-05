e1b66fea98da0ad1d2c3512e75ca83911cc171a1
sunchengchhay <sonchengchhay@gmail.com>
2025-03-05 20:11:29 +0700

    update

--- src/services/settingService.js
+++ src/services/settingService.js
@@ -10,4 +10,30 @@ const getSettingService = async ({res}) => {
     }
 }
 
-exports.getSettingService = getSettingService;
+const updateSettingService = async ({req, res}) => {
+    try {
+
+        const data = req.body
+
+        const setting = await Setting.findByPk(data.id);
+        if (!setting) {
+            return apiResponse(res, 404, 'Setting not found');
+        }
+
+        setting.theme = data.theme;
+        setting.shopName = data.shopName;
+        setting.shopLogo = data.shopLogo;
+        setting.font = data.font;
+
+        await setting.save();
+        return apiResponse(res, 200, 'Setting updated successfully', setting);
+    }
+    catch (error) {
+        return apiResponse(res, 500, error.message);
+    }
+}
+
+module.exports = {
+    getSettingService,
+    updateSettingService
+};
