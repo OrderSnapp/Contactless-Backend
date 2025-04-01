ef0414f8f6b7890741397be564fbdf0f734a3838
sunchengchhay <sonchengchhay@gmail.com>
2025-04-01 23:13:29 +0700

    update

--- src/app.js
+++ src/app.js
@@ -8,10 +8,13 @@ require('dotenv').config();
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cors({
   origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(
     session({
