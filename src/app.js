192c19b694a95f940b567763a9d4e721c3b3b5f6
sunchengchhay <sonchengchhay@gmail.com>
2025-05-16 22:25:06 +0700

    update

--- src/app.js
+++ src/app.js
@@ -10,11 +10,13 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-  // origin: 'http://localhost:3000',
   origin: ['http://localhost:3000', 'https://prod.sunchengchhay.me'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+app.options('*', cors());
 // app.use(cors({
 //   origin: function (origin, callback) {
 //     const allowed = ['http://localhost:3000', 'https://prod.sunchengchhay.me'];
