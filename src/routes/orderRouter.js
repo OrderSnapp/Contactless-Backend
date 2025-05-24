ae24c603cfedef6db07f333f6feaee99a446c77d
sunchengchhay <sonchengchhay@gmail.com>
2025-05-24 18:34:25 +0700

    update

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -4,14 +4,14 @@ const orderRouter = express.Router();
 const orderController = require('../controllers/orderController');
 const { authRoleMiddleware } = require('../middlewares/authMiddleware');
 
-orderRouter.post('', orderController.CreateOrder);
+orderRouter.post('', authRoleMiddleware(['Admin','Staff']), orderController.CreateOrder);
 orderRouter.post('/customer-orders', orderController.CreateCustomerOrder);
 orderRouter.get('/order-status/:status', orderController.GetAllOrderbyStatus);
 orderRouter.get('/progress-status', orderController.GetProgressOrderStatus);
 orderRouter.post('/update-status/:orderNumber', authRoleMiddleware(['Kitchen','Staff']), orderController.UpdateOrderStatus);
 orderRouter.get('/kitchen-status', authRoleMiddleware(['Kitchen']), orderController.GetAllKitchenStatus);
 orderRouter.get('/new-pending-order-notification', orderController.getNewPendingOrderNotification);
-orderRouter.get('/order-history', orderController.getOrderHistoryController);
+orderRouter.get('/order-history', authRoleMiddleware(['Admin','Staff']), orderController.getOrderHistoryController);
 orderRouter.get('/order-items/:id', orderController.getOrderDetailsController);
 
 module.exports = orderRouter;
