5ede6669b3998d1ad8c76e6009f71938f04c6fc6
sunchengchhay <sonchengchhay@gmail.com>
2025-05-28 22:43:24 +0700

    update

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -8,7 +8,7 @@ orderRouter.post('', authRoleMiddleware(['Admin','Staff']), orderController.Crea
 orderRouter.post('/customer-orders', orderController.CreateCustomerOrder);
 orderRouter.get('/order-status/:status', orderController.GetAllOrderbyStatus);
 orderRouter.get('/progress-status', orderController.GetProgressOrderStatus);
-orderRouter.post('/update-status/:orderNumber', authRoleMiddleware(['Kitchen','Staff']), orderController.UpdateOrderStatus);
+orderRouter.post('/update-status/:orderNumber', orderController.UpdateOrderStatus);
 orderRouter.get('/kitchen-status', authRoleMiddleware(['Kitchen']), orderController.GetAllKitchenStatus);
 orderRouter.get('/new-pending-order-notification', orderController.getNewPendingOrderNotification);
 orderRouter.get('/order-history', authRoleMiddleware(['Admin','Staff']), orderController.getOrderHistoryController);
